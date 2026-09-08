07c05670558f919a9c76d59ad9ab63d9339363a4
Aganze Munganga <chrisjoy5511@gmail.com>
2026-09-08 11:58:10 +0300

    Add files via upload

---
+++ All about me.py
@@ -0,0 +1,17 @@
+#My personl details 
+
+name = "Aganze Munganga"
+
+print (name)
+
+age = 19
+
+print (age)
+
+favoutite_language = "python"
+
+print(favoutite_language)
+
+my_goal = "Solve community problem with digital skills"
+
+print (my_goal)
